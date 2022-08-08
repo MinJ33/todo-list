d3cbe00c2b0eeeea661f445b1e17c5c2cb136e93
MinJ33 <erica990604@gmail.com>
2022-08-08 21:21:47 +0900

    feat: add ListingToDo function to add new To do list by getting values from newTodo

--- todo.js
+++ todo.js
@@ -3,12 +3,21 @@ const inputForm = document.querySelector("#input-form")
 const toDoInput = inputForm.querySelector("input")
 const todoLists = document.querySelector("#todo-lists")
 
+function LisitingToDo(newTodo){
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  todoLists.appendChild(li);
+}
+
 function handleToDoSubmit(event){
   event.preventDefault()
   // copying current value of the input to the new variable
   const newTodo = toDoInput.value;
   // empty the input form after submitting To Do
   toDoInput.value = "";
+  LisitingToDo(newTodo);
 }
 
 inputForm.addEventListener("submit", handleToDoSubmit)
